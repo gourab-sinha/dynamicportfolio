c6113496076d6cfd849a90a759eb0a24c3e483ba
gourab-sinha <gourab19964u@gmail.com>
2020-11-13 21:37:22 +0530

    fixed certification expire date issue

--- certifications/admin.py
+++ certifications/admin.py
@@ -4,6 +4,6 @@ from certifications.models import Certificate
 
 @admin.register(Certificate)
 class CertificateAdmin(admin.ModelAdmin):
-    list_display=('title', 'issued_by','slug', 'start_date', 'end_date', 'expire_date', 'is_lifetime_valid', 'image', 'verify_url', 'description', 'list_skills')
+    list_display=('title', 'issued_by','slug', 'start_date', 'expire_date', 'image', 'verify_url', 'description', 'list_skills')
     list_filter=('title', 'issued_by', 'start_date')
     prepopulated_fields = {'slug': ('title',)}
